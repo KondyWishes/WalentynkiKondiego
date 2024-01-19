6880fb0780bee5767df731be36806fc11ce69bc1
Konrad <kondykox@gmail.com>
2024-01-19 13:06:45 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
+  const [password, setPassword] = useState(null);
+
+  return (
+    <>
+      <header>
+        <h1 className="header__title">
+          Walentynki od Kondiego <span className="heart">❤</span>
+        </h1>
+        <img
+          className="kondi-heart"
+          src="./kondi_serce.png"
+          alt="Kondi Serduszko <3"
+        />
+        <div className="password">
+          <input
+            className="password__input"
+            type="text"
+            placeholder="Tu wpisz hasło..."
+          />
+          <button className="btn">No kliknij</button>
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default App
+export default App;
